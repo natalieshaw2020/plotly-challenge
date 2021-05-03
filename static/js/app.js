40d064316fe873022fd4fbbd5a2eca3ae89ef604
natalieshaw2020 <nshaw@heartlandcompany.com>
2021-05-03 12:51:18 -0500

    selection

---
+++ static/js/app.js
@@ -0,0 +1,5 @@
+// selections
+var idSelect = d3.select("#selDataset");
+var demographicsTable = d3.select("#sample-metadata");
+var barChart = d3.select("#bar");
+var bubbleChart = d3.select("bubble");
