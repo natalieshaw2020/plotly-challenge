e97c63dac2cb69e460c5473c7a89229f7e932549
natalieshaw2020 <nshaw@heartlandcompany.com>
2021-05-03 13:04:59 -0500

    dropdown

--- static/js/app.js
+++ static/js/app.js
@@ -3,3 +3,31 @@ var idSelect = d3.select("#selDataset");
 var demographicsTable = d3.select("#sample-metadata");
 var barChart = d3.select("#bar");
 var bubbleChart = d3.select("bubble");
+
+// dropdown menu
+function init() {
+    resetData();
+    // read in samples.json
+    d3.json("data/samples.json").then((data => {
+        data.names.forEach((name => {
+            var option = idSelect.append("option");
+            option.text(name);
+        })); 
+        // default id
+        var initId = idSelect.property("value")
+        plotCharts(initId);
+    })); 
+} 
+
+// reset
+function resetData() {
+    demographicsTable.html("");
+    barChart.html("");
+    bubbleChart.html("");
+}; 
+
+function optionChanged(id) {
+    resetData();
+    plotCharts(id);
+} 
+init();
